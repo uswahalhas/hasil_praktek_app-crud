529e886ca51d7a95a1c7cd211eee07e11f71eeac
uswahalhas <uswatnn55@gmail.com>
2024-10-31 17:28:42 +0700

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+
+class ProductController extends Controller
+{
+    public function index(){
+        $products = Product::all();
+        return view('products.index', ['products' => $products]);
+    }
+        public function create(){
+            return view('products.create');
+            }
+            public function store(Request $request){
+                $data = $request->validate([
+                    'name' => 'required',
+                    'qty' => 'required|numeric',
+                    'price' => 'required|numeric: 0,2',
+                    'description' => 'nullable'
+                ]);
+            
+                Product::create($data);
+            
+                return redirect(route('product.index'));
+            }
+            
+            public function edit(Product $product){
+                return view('products.edit', ['product' => $product]);
+            }
+
+            public function update(Product $product, Request $request){
+                $data = $request->validate([
+                    'name' => 'required',
+                    'qty' => 'required|numeric',
+                    'price' => 'required|numeric:0,2',
+                    'description' => 'nullable'
+                ]);
+
+            $product->update($data);
+
+                return redirect(route('product.index'))->with('success', 'Product Update Successfully');
+            }  
+            public function destroy(Product $product){
+                $product->delete();
+                return redirect(route('product.index'))->with('success', 'Product Deleted Succesfully');
+            }
+}   
